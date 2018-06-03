4c4ece89ee5f8f10351d6da5a41fe35f07b9ab00
Takeru Ohta <phjgt308@gmail.com>
2018-06-03 18:34:01 +0900

    Add top-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-extern crate hyparview;
-
+//! A Rust implementation of [Plumtree] algorithm.
+//!
+//! # References
+//!
+//! - [Plumtree: Epidemic Broadcast Trees][Plumtree]
+//!
+//! [Plumtree]: http://www.gsd.inesc-id.pt/~ler/reports/srds07.pdf
+#![warn(missing_docs)]
 pub use action::Action;
 pub use message::Message;
 pub use node::{Node, NodeOptions};
