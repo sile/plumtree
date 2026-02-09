148d2f1bdb86f6efd81a2ddfbe4ec9cb0de6a8a1
Takeru Ohta <phjgt308@gmail.com>
2026-02-09 20:41:26 +0900

    style: organize imports alphabetically

--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,10 @@
+use crate::System;
 use crate::action::{Action, ActionQueue};
 use crate::message::{
     GossipMessage, GraftMessage, IhaveMessage, Message, ProtocolMessage, PruneMessage,
 };
 use crate::missing::MissingMessages;
 use crate::time::{Clock, NodeTime};
-use crate::System;
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::time::Duration;
@@ -305,11 +305,12 @@ impl<T: System> Node<T> {
         self.eager_push_peers.insert(graft.sender.clone());
         self.lazy_push_peers.remove(&graft.sender);
         if let Some(message_id) = graft.message_id.take()
-            && let Some(payload) = self.messages.get(&message_id).cloned() {
-                let gossip =
-                    GossipMessage::new(&self.id, Message::new(message_id, payload), graft.round);
-                self.actions.send(graft.sender, gossip);
-            }
+            && let Some(payload) = self.messages.get(&message_id).cloned()
+        {
+            let gossip =
+                GossipMessage::new(&self.id, Message::new(message_id, payload), graft.round);
+            self.actions.send(graft.sender, gossip);
+        }
     }
 
     fn handle_prune(&mut self, prune: PruneMessage<T>) {
